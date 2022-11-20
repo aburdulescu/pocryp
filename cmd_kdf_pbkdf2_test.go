05d2668475aec8ac18645b1b2cf92b9908359c50
Andrei Burdulescu <andrei.burdulescu@pm.me>
2022-11-20 17:12:49 +0200

    add test for pbkdf2 command

---
+++ cmd_kdf_pbkdf2_test.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bytes"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func bytesFromHex(t testing.TB, s string) []byte {
+	r, err := hex.DecodeString(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return r
+}
+
+func TestCmdKdfPbkdf2(t *testing.T) {
+	type testvector struct {
+		p        []byte
+		s        []byte
+		c        int
+		dkLen    int
+		expected []byte
+	}
+	tvs := []testvector{
+		{
+			p:        []byte("password"),
+			s:        []byte("salt"),
+			c:        2,
+			dkLen:    20,
+			expected: bytesFromHex(t, "ea6c014dc72d6f8ccd1ed92ace1d41f0d8de8957"),
+		},
+		{
+			p:        []byte("passwordPASSWORDpassword"),
+			s:        []byte("saltSALTsaltSALTsaltSALTsaltSALTsalt"),
+			c:        4096,
+			dkLen:    25,
+			expected: bytesFromHex(t, "3d2eec4fe41c849b80c8d83662c0e44a8b291a964cf2f07038"),
+		},
+		{
+			p:        []byte{'p', 'a', 's', 's', 0, 'w', 'o', 'r', 'd'},
+			s:        []byte{'s', 'a', 0, 'l', 't'},
+			c:        4096,
+			dkLen:    16,
+			expected: bytesFromHex(t, "56fa6aa75548099dcc37d7f03425e0c3"),
+		},
+	}
+	tmp := t.TempDir()
+	out := filepath.Join(tmp, "out")
+	for i, tv := range tvs {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			f, err := os.Create(out)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer f.Close()
+			args := []string{
+				"-key", hex.EncodeToString(tv.p),
+				"-salt", hex.EncodeToString(tv.s),
+				"-iter", fmt.Sprintf("%d", tv.c),
+				"-len", fmt.Sprintf("%d", tv.dkLen),
+				"-hash=SHA-1",
+				"-out", out,
+			}
+			if err := cmdKdfPbkdf2(args); err != nil {
+				t.Fatal(err)
+			}
+			result, err := ioutil.ReadFile(out)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !bytes.Equal(tv.expected, result) {
+				t.Log("expected =", hex.EncodeToString(tv.expected))
+				t.Log("result   =", hex.EncodeToString(result))
+				t.Fatal("not equal")
+			}
+		})
+	}
+}
--- main.go
+++ main.go
@@ -25,7 +25,6 @@ import (
 
 	poaes "bandr.me/p/pocryp/internal/aes"
 	porsa "bandr.me/p/pocryp/internal/rsa"
-	"golang.org/x/crypto/pbkdf2"
 )
 
 func main() {
@@ -992,116 +991,6 @@ Options:
 		Bytes: x509.MarshalPKCS1PrivateKey(key),
 	}
 	if err := pem.Encode(w, block); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func cmdKdfPbkdf2(args []string) error {
-	fset := flag.NewFlagSet("kdf-pbkdf2", flag.ContinueOnError)
-	fset.Usage = func() {
-		fmt.Fprint(os.Stderr, `Usage: pocryp kdf-pbkdf2 -key|-key-file -salt|-salt-file -iter -len -hash [-out OUTPUT]
-
-Derive a new key from the given key using PBKDF2.
-
-If -in is not specified, stdin will be read.
-If -out is not specified, the output will be printed to stdout.
-
-Options:
-`)
-		fset.PrintDefaults()
-		os.Exit(1)
-	}
-
-	fOutput := fset.String("out", "", "Write the result to the file at path OUTPUT.")
-	fKey := fset.String("key", "", "Key as hex.")
-	fKeyFile := fset.String("key-file", "", "File which contains the key as binary/text.")
-	fSalt := fset.String("salt", "", "Salt as hex.")
-	fSaltFile := fset.String("salt-file", "", "File which contains the salt as binary/text.")
-	fIter := fset.Int("iter", 1024, "Number of iterations.")
-	fLen := fset.Int("len", 128, "Bit-length of the derived key.")
-	fHashFunc := fset.String("hash", "SHA-1", "Hash function(valid options: SHA-1, SHA-224, SHA-256, SHA-384 and SHA-512).")
-
-	fset.Parse(args)
-
-	if *fKey == "" && *fKeyFile == "" {
-		return errors.New("no key specified, use -key or -key-file to specify it")
-	}
-	if *fKey != "" && *fKeyFile != "" {
-		return errors.New("cannot use -key and -key-file at the same time")
-	}
-
-	if *fSalt == "" && *fSaltFile == "" {
-		return errors.New("no salt specified, use -salt or -salt-file to specify it")
-	}
-	if *fSalt != "" && *fSaltFile != "" {
-		return errors.New("cannot use -salt and -salt-file at the same time")
-	}
-
-	var hashFunc func() hash.Hash
-	switch *fHashFunc {
-	case "SHA-1":
-		hashFunc = sha1.New
-	case "SHA-224":
-		hashFunc = sha256.New224
-	case "SHA-256":
-		hashFunc = sha256.New
-	case "SHA-384":
-		hashFunc = sha512.New384
-	case "SHA-512":
-		hashFunc = sha512.New
-	default:
-		return errors.New("hash function is not valid")
-	}
-
-	var key []byte
-	if *fKey != "" {
-		b, err := hex.DecodeString(*fKey)
-		if err != nil {
-			return err
-		}
-		key = b
-	}
-	if *fKeyFile != "" {
-		b, err := ioutil.ReadFile(*fKeyFile)
-		if err != nil {
-			return err
-		}
-		key = b
-	}
-
-	var salt []byte
-	if *fSalt != "" {
-		b, err := hex.DecodeString(*fSalt)
-		if err != nil {
-			return err
-		}
-		salt = b
-	}
-	if *fSaltFile != "" {
-		b, err := ioutil.ReadFile(*fSaltFile)
-		if err != nil {
-			return err
-		}
-		salt = b
-	}
-
-	var w io.Writer
-	if *fOutput == "" {
-		w = os.Stdout
-	} else {
-		f, err := os.Create(*fOutput)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-		w = f
-	}
-
-	output := pbkdf2.Key(key, salt, *fIter, *fLen, hashFunc)
-
-	if _, err := io.Copy(w, bytes.NewBuffer(output)); err != nil {
 		return err
 	}
 
